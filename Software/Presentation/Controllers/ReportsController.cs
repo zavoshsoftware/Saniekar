0fc61de68462270eee9c5ad640d5cbc0fc836372
zavoshsoftware <70507208+zavoshsoftware@users.noreply.github.com>
2020-10-27 13:26:05 +0330

    StimulReport

---
+++ Software/Presentation/Controllers/ReportsController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Models;
+using Reports.Models;
+using Stimulsoft.Report;
+using Stimulsoft.Report.Mvc;
+
+namespace Presentation.Controllers
+{
+    public class ReportsController : Infrastructure.BaseControllerWithUnitOfWork
+    {
+        // GET: Report
+        public ActionResult Index(Guid id)
+        {
+            TempData["id"] = id;
+            return View();
+        }
+
+        public ActionResult LoadReportSnapshot()
+        {
+            Guid orderId = new Guid(TempData["id"].ToString());
+
+            var report = new StiReport();
+            report.Load(Server.MapPath("~/Reports/MRT/OrderListReport.mrt"));
+            report.RegBusinessObject("OrderObj", GetOrders(orderId));
+            //  report.Dictionary.Variables.Add("today", DateTime.Today());
+            return StiMvcViewer.GetReportResult(report);
+
+        }
+        public virtual ActionResult ViewerEvent()
+        {
+            return StiMvcViewer.ViewerEventResult();
+        }
+        public virtual ActionResult PrintReport()
+        {
+            return StiMvcViewer.PrintReportResult();
+        }
+
+        public virtual ActionResult ExportReport()
+        {
+            return StiMvcViewer.ExportReportResult();
+        }
+
+        public OrderReportViewModel GetOrders(Guid id)
+        {
+            Order order = UnitOfWork.OrderRepository.GetById(id);
+
+            OrderReportViewModel report = new OrderReportViewModel()
+            {
+                Code = order.Code.ToString(),
+                Description = order.Description,
+                RemainAmount = order.RemainAmountStr,
+                AdditiveAmount = order.AdditiveAmountStr,
+                Address = order.Address,
+                CustomerCellNumber = order.User.CellNum,
+                CustomerFullName = order.User.FullName,
+                Date = order.OrderDateStr,
+                DecreasedAmount = order.DiscountAmountStr,
+                DeliverDate = order.RecieveDateStr,
+                TotalAmount = order.TotalAmountStr,
+                TotalPayment = order.PaymentAmountStr,
+                OrderDetails = GetOrderDetails(id),
+                SubTotal = order.SubAmountStr
+            };
+
+
+            return report;
+        }
+
+        public IList<OrderDetailReportViewModel> GetOrderDetails(Guid orderId)
+        {
+            IList<OrderDetailReportViewModel> orderDetailReport=new List<OrderDetailReportViewModel>();
+
+            List<OrderDetail> orderDetails = UnitOfWork.OrderDetailRepository.Get(c => c.OrderId == orderId).ToList();
+
+            foreach (OrderDetail orderDetail in orderDetails)
+            {
+                string mattress = "-";
+                if (orderDetail.MattressId != null)
+                    mattress = orderDetail.Mattress.Title;
+
+                string color = "-";
+                if (orderDetail.ProductColorId != null)
+                    color = orderDetail.ProductColor.Title;
+
+                orderDetailReport.Add(new OrderDetailReportViewModel()
+                {
+                    Title = orderDetail.Product.Title,
+                    Amount = orderDetail.AmountStr,
+                    Quantity = orderDetail.Quantity.ToString(),
+                    RowAmount = orderDetail.RowAmountStr,
+                    ColorTitle = color,
+                    MattressTitle = mattress
+                });
+            }
+
+            return orderDetailReport;
+        }
+    }
+}
---
+++ Software/Presentation/Reports/Models/OrderReportViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Reports.Models
+{
+    public class OrderReportViewModel
+    {
+        public string Code { get; set; }
+        public string Date { get; set; }
+        public string TotalAmount { get; set; }
+        public string RemainAmount { get; set; }
+        public string CustomerFullName { get; set; }
+        public string Address { get; set; }
+        public string CustomerCellNumber { get; set; }
+        public string AdditiveAmount { get; set; }
+        public string DecreasedAmount { get; set; }
+        public string TotalPayment { get; set; }
+        public string Description { get; set; }
+        public string DeliverDate { get; set; }
+        public string SubTotal { get; set; }
+        public IList<OrderDetailReportViewModel> OrderDetails { get; set; }
+    }
+
+    public class OrderDetailReportViewModel
+    {
+        public string Title { get; set; }
+        public string Amount { get; set; }
+        public string Quantity { get; set; }
+        public string RowAmount { get; set; }
+        public string ColorTitle { get; set; }
+        public string MattressTitle { get; set; }
+
+    }
+}
